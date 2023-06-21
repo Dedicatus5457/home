fc09a15536cf4d1d5c2cf515971aa48fbcfc78ff
Qiyao Hou <ryanhou24@gmail.com>
2023-06-21 10:01:50 +0800

    index.js: siteStart time show false
    Signed-off-by: Qiyao Hou <ryanhou24@gmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ export const mainStore = defineStore("main", {
     return {
       innerWidth: null, // 当前窗口宽度
       coverType: "0", // 壁纸种类
-      siteStartShow: true, // 建站日期显示
+      siteStartShow: false, // 建站日期显示
       musicIsOk: false, // 音乐是否加载完成
       musicVolume: 0, // 音乐音量;
       musicOpenState: false, // 音乐面板开启状态
